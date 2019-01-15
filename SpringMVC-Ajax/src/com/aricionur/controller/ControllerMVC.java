ab0e8cec68d7967eabe6aaf8d61ad5ff6ceb3102
Onur Arıcı <aricionur@gmail.com>
2019-01-15 21:25:29 +0300

    SpringMVC/Ajax

---
+++ SpringMVC-Ajax/src/com/aricionur/controller/ControllerMVC.java
@@ -0,0 +1,65 @@
+package com.aricionur.controller;
+
+import com.aricionur.model.MongoDAO;
+import com.aricionur.model.Person;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ControllerMVC {
+	
+    
+    MongoDAO mongoDAO = MongoDAO.getInstance();
+    
+    
+////////////////////////////////////////////////////////////////////////////   
+    @RequestMapping(value = "/" , method = RequestMethod.GET)
+    public String index(ModelMap map) {
+        map.put("persons", mongoDAO.getMongodbAsPersonList());
+        map.put("pageHeader", "Person List");
+        mongoDAO.displayMongoDB();
+        return "index";
+        
+    }
+    
+////////////////////////////////////////////////////////////////////////////   
+  
+    @RequestMapping(value = "/addPerson" , method = RequestMethod.POST)
+    @ResponseBody
+    public Person addPerson(@RequestBody Person person) {
+        
+        Person localPerson =	mongoDAO.insert(person);
+        mongoDAO.displayMongoDB();
+  
+        return localPerson;
+    }
+    
+////////////////////////////////////////////////////////////////////////////
+    
+    @RequestMapping(value = "/updatePerson" , method = RequestMethod.PUT)
+    @ResponseBody
+    public String updatePerson(@RequestBody Person person) {
+        
+    	System.out.println("from ajax person info : "+ person.getName()+person.getSurname()+person.getPhoneNumber());
+       
+        mongoDAO.update(person);
+        mongoDAO.displayMongoDB();
+        
+        return "{\"status\":\"Success\"}";
+    }
+    
+////////////////////////////////////////////////////////////////////////////   
+    @RequestMapping(value = "/deletePerson" , method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deletePerson(@RequestBody Map<String, String> id) {
+    	mongoDAO.delete(Integer.parseInt(id.get("id")));
+    	mongoDAO.displayMongoDB();
+    }
+
+}
